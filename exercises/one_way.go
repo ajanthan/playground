521fafa59c4b247b3bf915b8cfa3d690826c5864
Ajanthan Balachandran <balaajanthan@gmail.com>
2020-11-10 22:09:58 -0800

    exe05 format

--- exercises/one_way.go
+++ exercises/one_way.go
@@ -1,1 +1,47 @@
 package exercises
+
+import (
+	"strings"
+)
+
+func IsOneAway(s1, s2 string) bool {
+	l1 := len(s1) //pale ple
+	l2 := len(s2)
+	if s1 == s2 {
+		return true
+	}
+	var newStr strings.Builder
+	if l1 > l2 {
+		//remove
+		for i := 0; i < len(s1); i++ {
+			newStr.Reset()
+			newStr.WriteString(s1[:i])
+			newStr.WriteString(s1[i+1:])
+			if s2 == newStr.String() {
+				return true
+			}
+		}
+	} else if l1 < l2 {
+		//insert
+		newStr.Reset()
+		for i := 0; i < len(s2); i++ {
+			newStr.WriteString(s2[:i])
+			newStr.WriteString(s2[i+1:])
+			if s1 == newStr.String() {
+				return true
+			}
+		}
+	} else {
+		//change
+		chnaged := 0
+		for i := 0; i < len(s2); i++ {
+			if s1[i] != s2[i] {
+				chnaged++
+			}
+		}
+		if chnaged == 1 {
+			return true
+		}
+	}
+	return false
+}
--- exercises/one_way_test.go
+++ exercises/one_way_test.go
@@ -1,43 +1,14 @@
 package exercises
 
-import "strings"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
-func IsOneAway(s1, s2 string) bool {
-	l1 := len(s1)
-	l2 := len(s2)
-	if s1 == s2 {
-		return true
-	}
-	var newStr strings.Builder
-	if l1 > l2 {
-		//remove
-		for i := 0; i < len(s1); i++ {
-			newStr.WriteString(s1[:i])
-			newStr.WriteString(s1[i+1:])
-			if s2 == newStr.String() {
-				return true
-			}
-		}
-	} else if l1 < l2 {
-		//insert
-		for i := 0; i < len(s2); i++ {
-			newStr.WriteString(s2[:i])
-			newStr.WriteString(s2[i+1:])
-			if s1 == newStr.String() {
-				return true
-			}
-		}
-	} else {
-		//change
-		chnaged := 0
-		for i := 0; i < len(s2); i++ {
-			if s1[i] != s2[i] {
-				chnaged++
-			}
-		}
-		if chnaged == 1 {
-			return true
-		}
-	}
-	return false
+func TestIsOneAway(t *testing.T) {
+	assert.True(t, IsOneAway("pale", "pale"))
+	assert.True(t, IsOneAway("pale", "ple"))
+	assert.True(t, IsOneAway("pales", "pale"))
+	assert.True(t, IsOneAway("pale", "bale"))
+	assert.False(t, IsOneAway("pale", "bake"))
 }
