dbef40ba7fa0c7e62eab920efc643353f939f71d
Ajanthan Balachandran <balaajanthan@gmail.com>
2020-11-11 20:53:55 -0800

    review exe01

--- exercises/is_unique.go
+++ exercises/is_unique.go
@@ -12,3 +12,30 @@ func IsUnique(s string) bool {
 	}
 	return true
 }
+
+func IsUniqueNoDS(s string) bool {
+	if len(s) > 128 {
+		return false
+	}
+	var chart [128]bool
+	for i := 0; i < len(s); i++ {
+		if chart[s[i]] {
+			return false
+		} else {
+			chart[s[i]] = true
+		}
+	}
+	return true
+}
+func IsUniqueLowerCases(s string) bool {
+	checker := 0
+	for i := 0; i < len(s); i++ {
+		index := s[i] - 'a'
+		if (checker & (1 << index)) > 0 {
+			return false
+		} else {
+			checker |= 1 << index
+		}
+	}
+	return true
+}
--- exercises/is_unique_test.go
+++ exercises/is_unique_test.go
@@ -19,3 +19,26 @@ func TestIsUnique(t *testing.T) {
 	assert.False(t, IsUnique("*dgkoanmlw*"))
 	assert.False(t, IsUnique("*dgkoanmlw1234567890-=qwertyuioasdgjlxcvnm"))
 }
+
+func TestIsUniqueNoDS(t *testing.T) {
+	assert.True(t, IsUniqueNoDS(""))
+	assert.True(t, IsUniqueNoDS(" "))
+	assert.True(t, IsUniqueNoDS("a"))
+	assert.True(t, IsUniqueNoDS("trieyu"))
+	assert.True(t, IsUniqueNoDS("4l1h3b"))
+	assert.True(t, IsUniqueNoDS("fg?056*&("))
+
+	assert.False(t, IsUniqueNoDS("aa"))
+	assert.False(t, IsUniqueNoDS("a12344rtt"))
+	assert.False(t, IsUniqueNoDS("*dgkoanmlw*"))
+	assert.False(t, IsUniqueNoDS("*dgkoanmlw1234567890-=qwertyuioasdgjlxcvnm"))
+}
+
+func TestIsUniqueLowerCases(t *testing.T) {
+	assert.True(t, IsUniqueLowerCases(""))
+	assert.True(t, IsUniqueLowerCases(" "))
+	assert.True(t, IsUniqueLowerCases("a"))
+	assert.True(t, IsUniqueLowerCases("trieyu"))
+	assert.False(t, IsUniqueLowerCases("abcda"))
+	assert.False(t, IsUniqueLowerCases("abcdab"))
+}
